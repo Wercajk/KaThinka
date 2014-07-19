349f6118f037b83ea7230e57ac55b0fa04f6ba86
Mario Vejlupek <mario@vejlupek.cz>
2014-07-19 13:22:40 +0200

    Introducing schema middleware option

--- test/app/resources/users.js
+++ test/app/resources/users.js
@@ -2,5 +2,12 @@ module.exports = {
   schema: {
     label: "String",
     url: "String"
+  },
+  schema_options:  {
+    middleware: function( schema ) {
+      schema.post('init', function ( doc ) {
+        console.log('%s has been initialized from the db', doc._id);
+      });
+    }
   }
 }
