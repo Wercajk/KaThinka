b51cb5456909b499839ab9b24a007b9e93a18ebc
Mario Vejlupek <mario@vejlupek.cz>
2014-08-16 20:41:44 +0200

    Adding doc automatic deployment

---
+++ bin/deploy-ghpages.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+rm -rf doc || exit 0;
+npm run doc
+( cd out
+ git init
+ git config user.name "Travis-CI"
+ git config user.email "travis@nodemeatspace.com"
+ git add .
+ git commit -m "Deploy doc to Github Pages"
+ git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+)
